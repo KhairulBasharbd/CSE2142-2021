1f1aab10d85fcbcad0b5f627d94c1db2412b18c9
Khairul Bashar <khairulbashar24853@gmail.com>
2021-12-06 11:26:30 +0600

    Task#10 : Add more comments and makes more naming improvements.

--- StudentList.java
+++ StudentList.java
@@ -3,7 +3,7 @@ import java.text.*;
 import java.util.*;
 public class StudentList {
 
-	
+	//This function is used to read a text file as string.
 	static String read(){
 		String fileText = null;
 		try {
@@ -13,6 +13,7 @@ public class StudentList {
 		return fileText;
 	}
 
+	//This function is used to write a string in text file.
 	static void write(String newStudent){
 
 		try {
@@ -35,50 +36,55 @@ public class StudentList {
 
 	public static void main(String[] args) {
 
-//		Check arguments
+		//		Check arguments
 		if (!args[0].equals("a") && !args[0].equals("r") &&  !args[0].contains("+") && !args[0].contains("?") && !args[0].contains("c")){
-			//System.out.println("You Entered wrong argument, Run Again ... ");
 			System.out.println("You Entered wrong argument, please Run Again ... ");
 			System.exit(0);
 
 		}
-		Constants cons = new Constants();
+		
+		Constants cons = new Constants();//creating object of Constants class.
 
+		//printing all single student of text file.
 		if(args[0].equals("a")) {
 			System.out.println(cons.loading);	
 
-			String students[] = read().split(",");			
+			String students[] = read().split(",");//spliting the text of file as single student.			
 			for(String student : students) { 
 				System.out.println(student); 
-			}
+			}//printing all single student of text file.
+
 			System.out.println(cons.loaded);
 		}
 
+		//printing a random student.
 		else if(args[0].equals("r")){
 			System.out.println(cons.loading);	
 
-			String students[] = read().split(",");	
-			Random x = new Random();
-			System.out.println(students[x.nextInt(students.length)]);
+			String students[] = read().split(",");	//spliting the text of file as single student.
+			Random x = new Random();//creating object of random class.
+			System.out.println(students[x.nextInt(students.length)]);//generate random index of student list and print random student.
 
 			System.out.println(cons.loaded);			
 		}
 
+		//add new student.
 		else if(args[0].contains("+")){
 			System.out.println(cons.loading);	
 
-			write(args[0].substring(1));
+			write(args[0].substring(1));;//calling write function to write any student in text file.
 							
 			System.out.println(cons.loaded);	
 		}
 
+		//search a student in text file.
 		else if(args[0].contains("?")){
 			System.out.println(cons.loading);	
 
-			String students[] = read().split(",");	
-
-			for(int idx = 0; idx<students.length; idx++) {
-				if(students[idx].trim().equals(args[0].substring(1))) {
+			String students[] = read().split(",");	//spliting the text of file as single student.
+			
+			for(int i = 0; i<students.length; i++) {
+				if(students[i].trim().equals(args[0].substring(1))) {
 					System.out.println("We found it!");	
 					
 				}
@@ -87,11 +93,12 @@ public class StudentList {
 			System.out.println(cons.loaded);				
 		}
 
+		//printing the number of student.
 		else if(args[0].contains("c")){
 			System.out.println(cons.loading);	
 
-			String students[] = read().split(",");	
-			System.out.println(students.length +" word(s) found ");
+			String students[] = read().split(",");	//spliting the text of file as single student.
+			System.out.println(students.length +" word(s) found ");//print the number of single student.
 
 			System.out.println(cons.loaded);				
 		}
